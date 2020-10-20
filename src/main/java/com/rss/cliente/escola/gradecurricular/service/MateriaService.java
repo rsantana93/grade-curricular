a24e9fa90a0a257eb9368105e089f84ca3f44c0b
rsantana93 <rikardinhosousa98@gmail.com>
2020-10-20 07:53:30 -0300

    Commit aula 11

--- src/main/java/com/rss/cliente/escola/gradecurricular/service/MateriaService.java
+++ src/main/java/com/rss/cliente/escola/gradecurricular/service/MateriaService.java
@@ -1,22 +1,26 @@
 package com.rss.cliente.escola.gradecurricular.service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import com.rss.cliente.escola.gradecurricular.controller.MateriaController;
 import com.rss.cliente.escola.gradecurricular.dto.MateriaDto;
 import com.rss.cliente.escola.gradecurricular.entity.MateriaEntity;
 import com.rss.cliente.escola.gradecurricular.exception.MateriaException;
 import com.rss.cliente.escola.gradecurricular.repository.IMateriaRepository;
 
 @Service
-public class MateriaService implements IMateriaServices {
+@CacheConfig(cacheNames = "materia")
+public class MateriaService implements IMateriaService {
 	
 	private static final String MENSAGEM_ERRO = "Erro interno identificado. Contate o suporte";
 	private static final String MATERIA_NAO_ENCONTRADA = "Matéria não encontrada";
@@ -72,9 +76,20 @@ public class MateriaService implements IMateriaServices {
 	}
 
 	@Override
+	@CachePut(unless = "#result.size()<3")
 	public List<MateriaDto> listar() {
 		try {
-			return this.mapper.map(this.materiaRepository.findAll(),new TypeToken<List<MateriaDto>>() {}.getType());
+			List<MateriaDto> materiaDto = this.mapper.map(this.materiaRepository.findAll(),
+					new TypeToken<List<MateriaDto>>() {
+					}.getType());
+
+			materiaDto.forEach(materia -> {
+				materia.add(WebMvcLinkBuilder
+						.linkTo(WebMvcLinkBuilder.methodOn(MateriaController.class).consultaMateria(materia.getId()))
+						.withSelfRel());
+			});
+
+			return materiaDto;
 		} catch (Exception e) {
 			throw new MateriaException(MENSAGEM_ERRO,
 					HttpStatus.INTERNAL_SERVER_ERROR);
@@ -83,6 +98,7 @@ public class MateriaService implements IMateriaServices {
 	}
 
 	@Override
+	@CachePut(key = "#id")
 	public MateriaDto consultar(Long id) {
 		try {
 			Optional<MateriaEntity> materiaOptional = this.materiaRepository.findById(id);
